c67fc1e660e340db12e6dc9276fbc8c1dd22d937
Won Geon <wonguns@naver.com>
2021-11-11 07:56:50 +0900

    update code

---
+++ 01_InputOutput/01_Input_Image.py
@@ -0,0 +1,19 @@
+
+import cv2 as cv
+import sys
+import os.path
+
+
+prjpath = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+print(prjpath)
+
+imgpath = prjpath + "\\10_Samples\starry_night.jpg"
+print(imgpath)
+
+img = cv.imread(imgpath)
+if img is None:
+    sys.exit("Could not read the image.")
+cv.imshow("Display window", img)
+k = cv.waitKey(0)
+if k == ord("s"):
+    cv.imwrite("starry_night.png", img)
